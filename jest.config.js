6469168515a6e5ae798e2c20ce12e63d4772c069
xsahxl <xsahxl@126.com>
2022-12-14 15:00:20 +0800

    fix: #7 dingtalk在yaml模式下md格式样式异常

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,9 @@ module.exports = {
   moduleFileExtensions: ['js', 'ts'],
   roots: ['<rootDir>/packages'],
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts'],
+  testMatch: [
+    '/Users/shihuali/workspace/serverless-cd/plugins/packages/ding-talk/__tests__/yaml/ding-talk-with-yaml.test.ts',
+  ],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
